23cc8993b21ab885ccec7be40cd4a62ca8f22aaa
Brad Davidson <brad.davidson@rancher.com>
2021-11-13 12:48:40 -0800

    Refactor registries package to support multiple endpoints
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/registries/registries_test.go
+++ pkg/registries/registries_test.go
@@ -2,9 +2,13 @@ package registries
 
 import (
 	"net/http"
+	"net/url"
+	"reflect"
 	"testing"
 
+	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/sirupsen/logrus"
 )
 
 func TestRewrite(t *testing.T) {
@@ -127,7 +131,7 @@ func TestRewrite(t *testing.T) {
 
 	for _, test := range rewriteTests {
 		registry := registry{
-			r: &Registry{
+			Registry: &Registry{
 				Mirrors: map[string]Mirror{
 					test.registry: {
 						Endpoints: []string{"https://registry.example.com/v2/"},
@@ -136,8 +140,7 @@ func TestRewrite(t *testing.T) {
 				},
 				Configs: map[string]RegistryConfig{},
 			},
-			t: map[string]*http.Transport{},
-			w: map[string]bool{},
+			transports: map[string]*http.Transport{},
 		}
 
 		for source, dest := range test.imageNames {
@@ -146,7 +149,7 @@ func TestRewrite(t *testing.T) {
 				t.Errorf("failed to parse source reference: %v", err)
 				continue
 			}
-			rewriteRef := registry.Rewrite(originalRef)
+			rewriteRef := registry.rewrite(originalRef)
 			if rewriteRef.Name() != dest {
 				t.Errorf("Bad rewrite for %s as %s - got %s, wanted %s", source, originalRef.Name(), rewriteRef.Name(), dest)
 			} else {
@@ -154,4 +157,185 @@ func TestRewrite(t *testing.T) {
 			}
 		}
 	}
+}
+
+func TestEndpoints(t *testing.T) {
+	type msr map[string]RegistryConfig
+	type msm map[string]Mirror
+
+	endpointTests := []struct {
+		imageName string
+		configs   msr
+		mirrors   msm
+		expected  []endpoint
+	}{
+		{ // no config, default endpoint
+			imageName: "busybox",
+			expected: []endpoint{
+				{url: mustParseURL("https://index.docker.io/v2")},
+			},
+		},
+		{ // config, but not for the registry we're pulling from
+			imageName: "busybox",
+			mirrors:   msm{"registry.example.com": Mirror{Endpoints: []string{"https://registry.example.com/v2"}}},
+			expected: []endpoint{
+				{url: mustParseURL("https://index.docker.io/v2")},
+			},
+		},
+		{ // config for docker.io, plus default endpoint
+			imageName: "busybox",
+			mirrors:   msm{"docker.io": Mirror{Endpoints: []string{"https://docker.example.com/v2"}}},
+			expected: []endpoint{
+				{url: mustParseURL("https://docker.example.com/v2")},
+				{url: mustParseURL("https://index.docker.io/v2")},
+			},
+		},
+		{ // multiple endpoints for docker.io, plus default endpoint
+			imageName: "busybox",
+			mirrors:   msm{"docker.io": Mirror{Endpoints: []string{"https://docker1.example.com/v2", "https://docker2.example.com/v2"}}},
+			expected: []endpoint{
+				{url: mustParseURL("https://docker1.example.com/v2")},
+				{url: mustParseURL("https://docker2.example.com/v2")},
+				{url: mustParseURL("https://index.docker.io/v2")},
+			},
+		},
+		{ // wildcard registry plus default
+			imageName: "busybox",
+			mirrors:   msm{"*": Mirror{Endpoints: []string{"https://registry.example.com/v2"}}},
+			expected: []endpoint{
+				{url: mustParseURL("https://registry.example.com/v2")},
+				{url: mustParseURL("https://index.docker.io/v2")},
+			},
+		},
+		{ // wildcard endpoint plus docker.io; only docker.io should be used
+			imageName: "busybox",
+			mirrors: msm{
+				"*":         Mirror{Endpoints: []string{"https://registry.example.com/v2"}},
+				"docker.io": Mirror{Endpoints: []string{"https://docker.example.com/v2"}},
+			},
+			expected: []endpoint{
+				{url: mustParseURL("https://docker.example.com/v2")},
+				{url: mustParseURL("https://index.docker.io/v2")},
+			},
+		},
+		{ // confirm that bad URLs are skipped
+			imageName: "busybox",
+			mirrors:   msm{"docker.io": Mirror{Endpoints: []string{"https://docker1.example.com/v2", "https://user:bad{@docker2.example.com"}}},
+			expected: []endpoint{
+				{url: mustParseURL("https://docker1.example.com/v2")},
+				{url: mustParseURL("https://index.docker.io/v2")},
+			},
+		},
+		{ // confirm that relative URLs are skipped
+			imageName: "busybox",
+			mirrors:   msm{"docker.io": Mirror{Endpoints: []string{"https://docker1.example.com/v2", "docker2.example.com/v2", "/v2"}}},
+			expected: []endpoint{
+				{url: mustParseURL("https://docker1.example.com/v2")},
+				{url: mustParseURL("https://index.docker.io/v2")},
+			},
+		},
+		{ // confirm that creds are used for the default endpoint
+			imageName: "busybox",
+			configs:   msr{"docker.io": RegistryConfig{Auth: &AuthConfig{Username: "user", Password: "pass"}}},
+			expected: []endpoint{
+				{
+					url:  mustParseURL("https://index.docker.io/v2"),
+					auth: &authn.Basic{Username: "user", Password: "pass"},
+				},
+			},
+		},
+		{ // confirm that creds are used for custom endpoints
+			imageName: "busybox",
+			mirrors:   msm{"docker.io": Mirror{Endpoints: []string{"https://docker1.example.com/v2"}}},
+			configs:   msr{"docker1.example.com": RegistryConfig{Auth: &AuthConfig{Username: "user", Password: "pass"}}},
+			expected: []endpoint{
+				{
+					url:  mustParseURL("https://docker1.example.com/v2"),
+					auth: &authn.Basic{Username: "user", Password: "pass"},
+				},
+				{
+					url: mustParseURL("https://index.docker.io/v2"),
+				},
+			},
+		},
+		{ // confirm that non-default schemes and ports are honored for mirrors and configs
+			imageName: "busybox",
+			mirrors:   msm{"docker.io": Mirror{Endpoints: []string{"http://docker1.example.com:5000/v2"}}},
+			configs:   msr{"docker1.example.com:5000": RegistryConfig{Auth: &AuthConfig{Username: "user", Password: "pass"}}},
+			expected: []endpoint{
+				{
+					url:  mustParseURL("http://docker1.example.com:5000/v2"),
+					auth: &authn.Basic{Username: "user", Password: "pass"},
+				},
+				{
+					url: mustParseURL("https://index.docker.io/v2"),
+				},
+			},
+		},
+	}
+
+	for _, test := range endpointTests {
+		t.Logf("Testing image %s with:\nmirrors: %#v\nconfigs: %#v", test.imageName, test.mirrors, test.configs)
+		registry := registry{
+			Registry: &Registry{
+				Mirrors: test.mirrors,
+				Configs: test.configs,
+			},
+			transports: map[string]*http.Transport{},
+		}
+
+		ref, err := name.ParseReference(test.imageName)
+		if err != nil {
+			t.Errorf("Failed to parse test refrence: %v", err)
+			continue
+		}
+
+		endpoints, err := registry.getEndpoints(ref)
+		if err != nil {
+			t.Errorf("Failed to get endpoints for %s: %v", ref, err)
+			continue
+		}
+		if expected, got := len(test.expected), len(endpoints); expected != got {
+			t.Errorf("Expected %d endpoints, got %d", expected, got)
+			continue
+		}
+		for i, endpoint := range endpoints {
+			if test.expected[i].url.String() != endpoint.url.String() {
+				t.Errorf("Expected endpoint[%d] url %v, got %v", i, test.expected[i].url, endpoint.url)
+				continue
+			}
+
+			expectedAuth, err := getAuthConfig(test.expected[i], ref)
+			if err != nil {
+				t.Errorf("Failed to get auth for expected endpoint: %v", err)
+				continue
+			}
+
+			epAuth, err := getAuthConfig(endpoint, ref)
+			if err != nil {
+				t.Errorf("Failed to get auth for test endpoint: %v", err)
+				continue
+			}
+
+			if !reflect.DeepEqual(expectedAuth, epAuth) {
+				t.Errorf("Expected endpoint[%d] auth %#v, got %#v", i, expectedAuth, epAuth)
+			}
+		}
+	}
+}
+
+func getAuthConfig(resolver authn.Keychain, ref name.Reference) (*authn.AuthConfig, error) {
+	auth, err := resolver.Resolve(ref.Context())
+	if err != nil {
+		return nil, err
+	}
+	return auth.Authorization()
+}
+
+func mustParseURL(s string) *url.URL {
+	u, err := url.Parse(s)
+	if err != nil {
+		logrus.Fatalf("Failed to parse url %s: %v", s, err)
+	}
+	return u
 }
